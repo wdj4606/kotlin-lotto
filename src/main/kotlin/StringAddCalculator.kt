f13c9dfb795e0fa6b7ff467e9daf5ef68840ebe2
rtf400 <rtf400@naver.com>
2024-07-24 00:32:17 +0900

    <추가> (Step1) : 문자열 덧셈 계산기
    음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)

--- src/main/kotlin/StringAddCalculator.kt
+++ src/main/kotlin/StringAddCalculator.kt
@@ -8,6 +8,7 @@ class StringAddCalculator {
 
         val trySingleInt = parser.convertSingleInteger(text!!)
         trySingleInt?.let {
+            require(trySingleInt >= 0) { MSG_EXCEPTION_NEGATIVE }
             return trySingleInt
         }
 
@@ -26,51 +27,12 @@ class StringAddCalculator {
         }
 
         return sum
-
-//        if (isSingleInteger(text)) {
-//            return checkPostiveIntegerAndReturn(text)
-//        }
-//
-//        var finalText: String = text
-//        val resultPair = getDelimiterAndRemainingText(text)
-//        if (resultPair.first != null && resultPair.second != null) {
-//            finalText = resultPair.second!!
-//        }
     }
 
-//    private fun checkPostiveIntegerAndReturn(text: String): Int {
-//        val positive = text.toInt()
-//        check(positive >= 0) { throw NumberFormatException(MSG_EXCEPTION_NEGATIVE) }
-//        return positive
-//    }
-//
-//    private fun getDelimiterAndRemainingText(text: String): Pair<String?, String?> {
-//        val result = Regex("//(.)\n(.*)").find(text)
-//        val delimiter = result?.groupValues?.get(1)
-//        val remainingText = result?.groupValues?.get(2)
-//        return Pair(delimiter, remainingText)
-//    }
-//
-//    private fun splitAndAdd(text: String, delimiter: String?): Int {
-//        val delimiters = mutableListOf("[,:]")
-//
-//        delimiter?.let {
-//            delimiters.add(Regex.escape(it))
-//        }
-//
-//        val combinedRegex = delimiters.joinToString(separator = "|") { it }
-//        val numbers = text.split(combinedRegex.toRegex())
-//
-//        var sum = 0
-//        for (number in numbers) {
-//            require(isSingleInteger(number)) { MSG_EXCEPTION_FORMULA_ERROR }
-//            sum += checkPostiveIntegerAndReturn(number)
-//        }
-//        return sum
-//    }
-//
+    
+
     companion object {
-//        const val MSG_EXCEPTION_NEGATIVE = "음수는 허용되지 않습니다."
+        const val MSG_EXCEPTION_NEGATIVE = "음수는 허용되지 않습니다."
         const val MSG_EXCEPTION_FORMULA_ERROR = "수식이 잘못되었습니다."
     }
 }
