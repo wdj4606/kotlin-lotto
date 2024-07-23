a25ee00b50fc63118cd0aa664bb506b65d1b9204
rtf400 <rtf400@naver.com>
2024-07-23 21:50:51 +0900

    <추가> (Step1) : 문자열 덧셈 계산기
    덧셈 계산기 구현 및 테스트 코드 추가

---
+++ src/main/kotlin/StringAddCalculator.kt
@@ -0,0 +1,68 @@
+fun main () {
+    val calculator = StringAddCalculator()
+    val result = calculator.add("")
+    println(result)
+}
+
+class StringAddCalculator {
+    fun add(text: String?): Int {
+        if (text.isNullOrEmpty()) return 0
+        if (text.isBlank()) return 0
+
+        if (isSingleInteger(text)) {
+            return checkPostiveIntegerAndReturn(text)
+        }
+
+        var finalText: String = text
+        val resultPair = getDelimiterAndRemainingText(text)
+        if (resultPair.first != null && resultPair.second != null) {
+            finalText = resultPair.second!!
+        }
+        return splitAndAdd(finalText, resultPair.first)
+    }
+
+    private fun isSingleInteger(text: String): Boolean {
+        try {
+            text.toInt()
+            return true
+        } catch (e: NumberFormatException) {
+            return false
+        }
+    }
+
+    private fun checkPostiveIntegerAndReturn(text: String): Int {
+        val positive = text.toInt()
+        check(positive >= 0) { throw NumberFormatException(MSG_EXCEPTION_NEGATIVE) }
+        return positive
+    }
+
+    private fun getDelimiterAndRemainingText(text: String): Pair<String?, String?> {
+        val result = Regex("//(.)\n(.*)").find(text)
+        val delimiter = result?.groupValues?.get(1)
+        val remainingText = result?.groupValues?.get(2)
+        return Pair(delimiter, remainingText)
+    }
+
+    private fun splitAndAdd(text: String, delimiter: String?): Int {
+        val delimiters = mutableListOf("[,:]")
+
+        delimiter?.let {
+            delimiters.add(Regex.escape(it))
+        }
+
+        val combinedRegex = delimiters.joinToString(separator = "|") { it }
+        val numbers = text.split(combinedRegex.toRegex())
+
+        var sum = 0
+        for (number in numbers) {
+            require(isSingleInteger(number)) { MSG_EXCEPTION_FORMULA_ERROR }
+            sum += checkPostiveIntegerAndReturn(number)
+        }
+        return sum
+    }
+
+    companion object {
+        const val MSG_EXCEPTION_NEGATIVE = "음수는 허용되지 않습니다."
+        const val MSG_EXCEPTION_FORMULA_ERROR = "수식이 잘못되었습니다."
+    }
+}
