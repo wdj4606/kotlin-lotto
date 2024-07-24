0f378df757f29a3f1557d1d3132655fe55090c8e
rtf400 <rtf400@naver.com>
2024-07-24 22:40:25 +0900

    <추가> (Step2) : 로또
    로또 자판기는 지급된 금액을 1000으로 나눈 몫만큼 로또 티켓을 발급한다.

---
+++ src/main/kotlin/lotto/LottoVendingMachine.kt
@@ -0,0 +1,14 @@
+package lotto
+
+object LottoVendingMachine {
+    const val PRICE = 1000
+
+    fun purchase(money: Int): MutableList<LottoTicket>{
+        val count = money / PRICE
+        val tickets = mutableListOf<LottoTicket>()
+        repeat(count) {
+            tickets.add(LottoTicket())
+        }
+        return tickets
+    }
+}
