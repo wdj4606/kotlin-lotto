e0166748ce1e7310f34d79ec65cedee238b80d44
홍진 박 <phj0319@midasit.co.kr>
2024-07-24 14:08:35 +0900

    <코드리뷰> (Step1) : 문자열 덧셈 계산기
    누락된 MD 수정 및 테스트 문구 주체화

--- src/test/kotlin/StringValidatorTest.kt
+++ src/test/kotlin/StringValidatorTest.kt
@@ -5,7 +5,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.provider.ValueSource
 
 class StringValidatorTest {
-    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 True를 반환해야 한다.")
     @ParameterizedTest
     @NullAndEmptySource
     fun emptyOrNull(text: String?) {
