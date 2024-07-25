47c5258ae50e5fe3d12d951fc393254e026e8888
pcs0807 <pcs0807@midasit.com>
2024-07-25 22:07:42 +0900

    <step2> 당첨 번호를 입력받는다.

--- src/main/kotlin/Lotto/view/InputView.kt
+++ src/main/kotlin/Lotto/view/InputView.kt
@@ -6,5 +6,10 @@ class InputView {
             println("구입금액을 입력해 주세요.")
             return readlnOrNull()?.toInt() ?: inputMoney()
         }
+
+        fun inputWinningNumber(): List<Int> {
+            println("지난 주 당첨 번호를 입력해 주세요.")
+            return readlnOrNull()?.split(",")?.map { it.toInt() } ?: inputWinningNumber()
+        }
     }
 }
