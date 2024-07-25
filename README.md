031d73e1850260bd5d585b2aeba42de637dcae5a
pcs0807 <pcs0807@midasit.com>
2024-07-25 22:03:44 +0900

    <step2> 로또 구입 금액을 입력받는다.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# kotlin-lotto# kotlin-lotto
+# kotlin-lotto
 
 로또 게임을 구현해 학습하는 프로젝트입니다.
 
 ## 기능 요구사항
+- 로또 구입 금액을 입력받는다.
--- src/main/kotlin/Lotto/view/InputView.kt
+++ src/main/kotlin/Lotto/view/InputView.kt
@@ -1,4 +1,10 @@
 package Lotto.view
 
 class InputView {
+    companion object {
+        fun inputMoney(): Int {
+            println("구입금액을 입력해 주세요.")
+            return readlnOrNull()?.toInt() ?: inputMoney()
+        }
+    }
 }
